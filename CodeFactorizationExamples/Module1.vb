2f519312094f249479827813ba49916a3995ea15
Rodolphe Saugier <rodolphe.saugier@gmail.com>
2019-02-21 15:22:56 +0100

    step1

--- CodeFactorizationExamples/Module1.vb
+++ CodeFactorizationExamples/Module1.vb
@@ -13,13 +13,8 @@ Module Module1
                 Next
             Next
         Next
-        Console.WriteLine("Magic days since 1980 are:")
-        For Each result In results
-            Console.Write(result.ToString("d/M/y "))
-        Next
-        Console.WriteLine("A total of " & results.Count)
-        Console.WriteLine("Press a key")
-        Console.ReadKey()
+
+        DisplayResults("Magic days since 1980 are:", results, DisplayMode.Simple)
 
         results.Clear()
         For Each y In Enumerable.Range(1980, 40)
@@ -32,14 +27,8 @@ Module Module1
                 Next
             Next
         Next
-        Console.WriteLine("Super-magic days since 1980 are:")
-        For Each result In results
-            Console.Write(result.DayOfWeek.ToString & " " & result.ToString("d/M/y") & " ")
-        Next
-        Console.WriteLine()
-        Console.WriteLine("A total of " & results.Count)
-        Console.WriteLine("Press a key")
-        Console.ReadKey()
+
+        DisplayResults("Super-magic days since 1980 are:", results, DisplayMode.WithDayOfWeek)
 
         results.Clear()
         For Each y In Enumerable.Range(1980, 40)
@@ -52,14 +41,8 @@ Module Module1
                 Next
             Next
         Next
-        Console.WriteLine("Supa-dupa-magic days since 1980 are:")
-        For Each result In results
-            Console.Write(result.DayOfWeek.ToString & " " & result.ToString("d/M/y") & " ")
-        Next
-        Console.WriteLine()
-        Console.WriteLine("A total of " & results.Count)
-        Console.WriteLine("Press a key")
-        Console.ReadKey()
+
+        DisplayResults("Supa-dupa-magic days since 1980 are:", results, DisplayMode.WithDayOfWeek)
 
         results.Clear()
         For Each y In Enumerable.Range(1, 9999)
@@ -71,14 +54,8 @@ Module Module1
                 Next
             Next
         Next
-        Console.WriteLine("Ultimate magic days are:")
-        For Each result In results
-            Console.Write(result.DayOfWeek.ToString & " " & result.ToString("d/M/y") & " ")
-        Next
-        Console.WriteLine()
-        Console.WriteLine("A total of " & results.Count)
-        Console.WriteLine("Press a key")
-        Console.ReadKey()
+
+        DisplayResults("Ultimate magic days are:", results, DisplayMode.Simple)
 
         results.Clear()
         For Each y In Enumerable.Range(1, 9999)
@@ -91,15 +68,29 @@ Module Module1
                 Next
             Next
         Next
-        Console.WriteLine("Almost ultimate magic days are:")
-        For Each result In results
-            Console.Write(result.DayOfWeek.ToString & " " & result.ToString("d/M/yyyy") & " ")
-        Next
-        Console.WriteLine()
-        Console.WriteLine("A total of " & results.Count)
-        Console.WriteLine("Press a key")
-        Console.ReadKey()
+
+        DisplayResults("Almost ultimate magic days are:", results, DisplayMode.WithDayOfWeek)
 
     End Sub
 
+    Public Enum DisplayMode
+        Simple
+        WithDayOfWeek
+    End Enum
+
+    Private Sub DisplayResults(message As String, results As List(Of Date), display As DisplayMode)
+        Console.WriteLine(message)
+        For Each result In results
+            Dim text As String
+            If display = DisplayMode.WithDayOfWeek Then
+                text = result.DayOfWeek.ToString & " " & result.ToString("d/M/yyyy ")
+            Else
+                text = result.ToString("d/M/yyyy ")
+            End If
+            Console.Write(text)
+        Next
+        Console.WriteLine("A total of " & results.Count)
+        Console.WriteLine("Press a key")
+        Console.ReadKey()
+    End Sub
 End Module
